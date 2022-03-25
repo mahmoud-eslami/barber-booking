fa3290a03ef177751818df9770d619663eb049db
mahmoud <eslamimahmoud98@gmail.com>
2022-03-25 20:55:08 +0430

    barber shop added

---
+++ lib/app/data/model/barber_shop/barber_shop.dart
@@ -0,0 +1,25 @@
+class BarberShop {
+  final String title;
+  final String subTitle;
+  final List<String> tags;
+  final String description;
+  final double lat, long;
+
+  BarberShop({
+    required this.title,
+    required this.subTitle,
+    required this.tags,
+    required this.description,
+    required this.lat,
+    required this.long,
+  });
+
+  factory BarberShop.fromJson(doc) => BarberShop(
+        title: doc["title"],
+        subTitle: doc["subTitle"],
+        tags: doc["tags"],
+        description: doc["description"],
+        lat: doc["lat"],
+        long: doc["long"],
+      );
+}
