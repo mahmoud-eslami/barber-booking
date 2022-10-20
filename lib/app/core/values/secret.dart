e501a610d2ca91b4051fb08b2ad07b9440ec85c3
mahmoud <eslamimahmoud98@gmail.com>
2022-10-20 19:11:22 +0330

    secret file added

---
+++ lib/app/core/values/secret.dart
@@ -0,0 +1,4 @@
+var mapBoxUrl =
+    "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFobW91ZC1lc2xhbWkiLCJhIjoiY2wwazJhb3g5MGljNzNwa2NqcTB2cDdtbiJ9.mMwvYfEnD_YY44C4njkRzw";
+var darkMapUrl =
+    "https://api.mapbox.com/styles/v1/mahmoud-eslami/cl0k2k3hc001e15qs1qzzmdsn/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFobW91ZC1lc2xhbWkiLCJhIjoiY2wwazJhb3g5MGljNzNwa2NqcTB2cDdtbiJ9.mMwvYfEnD_YY44C4njkRzw";
